7d105c99ddfc87e36892f9fb0946f0e470c16a46
Raphael <pierrotraphael08@gmail.com>
2024-11-26 14:32:01 +0100

    ajout pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,39 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        name: "DreamRecord",
+        short_name: "DreamRecord",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#4A90E2",
+        icons: [
+          {
+            src: "src/assets/icon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "src/assets/icon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,svg,ts,scss}"],
+      },
+    }),
+  ],
   optimizeDeps: {
     include: ["aos"],
   },
